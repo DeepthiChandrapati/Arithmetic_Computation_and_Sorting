4664e6187f621d05d90cf8ba738a165de74bd2df
Ch Deepthi <chandrapatideepthi@gmail.com>
2020-06-03 15:47:14 +0530

    Added: First Computation

--- ArthmticCmputatns.sh
+++ ArthmticCmputatns.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 echo "Welcome to Arithmetic Computations Program "
-read -p "Enter a:  "a
-read -p "Enter b:  "b
-read -p "Enter c:  "c
-
+read -p "Enter a:  " a
+read -p "Enter b:  " b
+read -p "Enter c:  " c
+FirstComputation=$(( $a + $b * $c ));
+echo $FirstComputation
