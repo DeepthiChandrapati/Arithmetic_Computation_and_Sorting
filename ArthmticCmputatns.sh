1acc6175ff036f937571ff989e60b4110ea4104d
Ch Deepthi <chandrapatideepthi@gmail.com>
2020-06-03 16:15:44 +0530

    Added: Second Computation

--- ArthmticCmputatns.sh
+++ ArthmticCmputatns.sh
@@ -7,6 +7,9 @@ read -p "Enter b:  " b
 read -p "Enter c:  " c
 
 FirstComputation=$(( $a + $b * $c ));
+
 SecondComputation=$(( $a * $b + $c ));
+
 echo "First Computation is : $FirstComputation"
+
 echo "Second Computation is : $SecondComputation"
